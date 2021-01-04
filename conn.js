29057d3cee001cd8de85a476f34ab81bf6b4f5d8
Araf Al <49306997+rocketjawhead@users.noreply.github.com>
2021-01-04 14:42:06 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host : 'localhost',
+    host : '127.0.0.1',
+    port : '3306',
     user : 'root',
     password : 'root',
     database : 'express'
